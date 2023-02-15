03b12c9956203cdc1ef586112e232b732fb9ead3
Sarah Stephenson <123026466+sstephenson01@users.noreply.github.com>
2023-02-15 15:59:05 -0500

    Add files via upload

---
+++ L44.py
@@ -0,0 +1,8 @@
+# Breanna Eafon and Sarah Stephenson
+for x in ["paper","rock","paper","scissor","rock","rock","scissors","paper"]:
+    if x=="paper":
+        print("paper eats rock")
+    if x=="scissors":
+        print("scissor beats paper")
+    if x=="rock":
+        print("rock hits scissors")
